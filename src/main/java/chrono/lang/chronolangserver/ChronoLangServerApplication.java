329ecced95d9025cd0c03017348077b45e6bbd5e
MaxKostov <max.kostov04@gmail.com>
2025-03-29 01:28:40 +0200

    first commit

---
+++ src/main/java/chrono/lang/chronolangserver/ChronoLangServerApplication.java
@@ -0,0 +1,13 @@
+package chrono.lang.chronolangserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChronoLangServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChronoLangServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/chrono/lang/chronolangserver/ChronoLangServerApplicationTests.java
@@ -0,0 +1,13 @@
+package chrono.lang.chronolangserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChronoLangServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
